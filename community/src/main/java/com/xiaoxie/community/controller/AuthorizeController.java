cdf51fc4d0b68d238293ec52d315cc23ef9c7792
254652062@qq.com <254652062@qq.com>
2019-11-09 22:17:22 +0800

    完成发布页面

--- community/src/main/java/com/xiaoxie/community/controller/AuthorizeController.java
+++ community/src/main/java/com/xiaoxie/community/controller/AuthorizeController.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
+
 @Controller
 public class AuthorizeController {
     @Autowired
